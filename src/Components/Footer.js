853c9d534d8a611f8b557cfe67f65d56cc77a7bf
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-04-04 15:38:05 -0700

    Init

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <section className="footer">
+        <div className="footer-text">
+          <p>
+            © 2022-2023 ZOA Energy LLC. {""} Developed by: Jonathan Plaisted
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
