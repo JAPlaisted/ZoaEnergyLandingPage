86202afae2875acb3340d0199fbb59b5f036dc70
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-04-05 11:16:45 -0700

    Mobile Layout

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -42,7 +42,12 @@ function Header() {
             id="dark-mode-toggle"
             className="dark-mode-toggle"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 496">
+            <svg
+              width="20px"
+              height="20px"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 496 496"
+            >
               <path
                 fill="currentColor"
                 d="M8,256C8,393,119,504,256,504S504,393,504,256,393,8,256,8,8,119,8,256ZM256,440V72a184,184,0,0,1,0,368Z"
