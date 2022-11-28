7b2838cd246c763e56331782bee6c98851b47ef9
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-11-28 12:08:47 -0800

    Added banner and zoa can on mobile

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./Components/Header";
 import Information from "./Components/Information";
 import Footer from "./Components/Footer";
+import Banner from "./Components/Banner";
 
 function App() {
   return (
     <div className="App ">
       <Header />
       <Information />
+      <Banner />
       <Footer />
     </div>
   );
---
+++ src/Components/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div>
+      <section className="banner">
+        <h2>DRINK ZOA</h2>
+      </section>
+    </div>
+  );
+}
+
+export default Banner;
